bd2bd06673df5d2c5615119304675069fb83b5f3
dev-bot1 <kshitij_201900328@smit.smu.edu.in>
2021-08-07 12:38:17 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd1784dc9d4af5a5611c6df908629627'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5698e8db-0466-497e-99c6-c167239190f9'
+};
